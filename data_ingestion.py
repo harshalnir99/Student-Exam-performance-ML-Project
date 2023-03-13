a2e5a9a5e0549f1e29c46f61c3ea183d64c77703
Harshal Nirgulkar <harshalnir99@gmail.com>
2023-03-13 16:02:10 +0530

    Ingestion,Transformastion & Training Done

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,15 +1,17 @@
 import os
 import sys
-import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from data_transformation import DataTransformation
 from data_transformation import DataTransformationConfig
 
-
+from model_trainer import ModelTrainerConfig
+from model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -37,7 +39,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data is completed")
+            logging.info("Ingestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -52,5 +54,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
